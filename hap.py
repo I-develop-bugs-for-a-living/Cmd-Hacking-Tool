1458f4e6839ab2051a97031c65fa2cf0ec89eba0
I-develop-bugs-for-a-living <jan.buehne@outlook.com>
2023-04-26 15:06:52 +0200

    Command Dict

--- hap.py
+++ hap.py
@@ -1,5 +1,16 @@
 import os
 import sys
+import subprocess
+
+# answer sheet
+responsDict = {}
+with open('responses.txt', 'r') as f:
+    responses = f.readlines()
+    for response in responses:
+        response = response.strip()
+        if response != '':
+            responsDict[response.split(':')[0]] = response.split(':')[1]
+    print(responsDict)
 
 os.system('cls')
 while True:
@@ -7,5 +18,13 @@ while True:
     sys.stdout.write(os.path.abspath(__file__))
     sys.stdout.write(">")
     a = input("")
-    if a == "cd ..":
-        sys.stdout.write('Are you stupid?\n')
+    cmd = a.split(" ")
+    if cmd[0] == "exit":
+        break
+
+    run = subprocess.run(["cd", ".."], capture_output=True, shell=True)
+
+    print(run.stdout) # the output "Test"
+    print(run.stderr) # the error part of the output
+
+    
